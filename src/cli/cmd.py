9aeaf62d647fbcb40b6c04ad733171d224d5356e
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-25 19:18:44 -0300

    Add cli commands

--- src/cli/cmd.py
+++ src/cli/cmd.py
@@ -1,8 +1,15 @@
-##### SIMIAN COMMAND LINE INTERFACE #####
-import sys
+import argparse
+import os
 
 
 def main():
-    args = sys.argv[1:]
-    if len(args) < 1:
-        print("usage: ...")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('option', help='siamian options')
+    parser.add_argument('arg_option', help='name of your game')
+
+    args = parser.parse_args()
+    if args.option == 'startgame':
+        print(f'creating new game project {args.game_name}')
+    elif args.option == 'rungame':
+        print(f'running {args.arg_option}')
+        os.system(f'python {args.arg_option}')
