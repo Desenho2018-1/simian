9ebc6acece8ae7b457ddf48152ac195c3604bfb2
rodrigocam <rodrigo.redcode@gmail.com>
2018-05-30 19:53:33 -0300

    Add placeholder for method update and draw

--- simian/gameobject/game_object.py
+++ simian/gameobject/game_object.py
@@ -9,7 +9,7 @@ class Position:
     A position represents a point in a 2D space.
     """
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=None, y=None):
         self.x = x
         self.y = y
 
@@ -34,6 +34,25 @@ class GameObject:
     def __init__(self, x=None, y=None, height=None, width=None):
         self.pos = Position(x, y)
         self.size = Size(height, width)
+
+    def update(self):
+        """
+        This method is called every frame of the game loop.
+        All the logic that changes something in a game object goes here.
+        E.g. a square that change his color every 5 seconds, your code
+        could be something like
+            >>> def update(self):
+                    if its_time:
+                        self.color = next_color(self.color)
+        """
+        pass
+    
+    def draw(self, renderable_object):
+        """
+        This method is called every frame of the game loop.
+        Draw the renderable object in the position of this game object.
+        """
+        pass
 
     def clone(self):
         """
