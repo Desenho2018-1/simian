dda624c0b511e1f0a408c0ec07e4f79afd217f07
pablodiegoss <pablodiegoss@hotmail.com>
2018-06-01 19:20:31 -0300

    Update Template Game

--- template_game/game.py
+++ template_game/game.py
@@ -1,7 +1,15 @@
 from simian.game_engine import GameEngine
+from template_game.template_scene import TemplateScene
 
 engine = GameEngine()
 
-# TODO: Use engine.addScene to add scenes to the scene manager before running the game
+engine.load()
+
+scene1 = TemplateScene("Fase1", 0)
+scene2 = TemplateScene("Fase2", 1)
+scene3 = TemplateScene("Fase3", 2)
+
+engine.add_scene(scene1,scene2,scene3)
+
 
 engine.run()
