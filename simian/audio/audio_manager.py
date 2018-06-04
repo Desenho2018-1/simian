af11ef2b71c5e460315d29706515afefbf917e68
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-04 20:00:15 -0300

    Fix audio manager imports
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- simian/audio/audio_manager.py
+++ simian/audio/audio_manager.py
@@ -1,10 +1,11 @@
 import pygame
-from audios import Audio, Music, Sound
+from simian.audio.audios import Audio, Music, Sound
 
 class AudioManager:
     def __init__(self):
         pygame.mixer.init()
         self.audios = []
+        
     def pause_all(self):
         pygame.mixer.pause()
 
