8784eeee3736825b9b48296941dd60c56feae800
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-04 20:05:59 -0300

    Create play music method for audio manager class
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- simian/audio/audio_manager.py
+++ simian/audio/audio_manager.py
@@ -5,9 +5,14 @@ class AudioManager:
     def __init__(self):
         pygame.mixer.init()
         self.audios = []
-        
+        self.music = None
+
     def pause_all(self):
         pygame.mixer.pause()
 
     def unpause_all(self):
         pygame.mixer.unpause()
+
+    def play_music(self, music_path):
+        self.music = Music(music_path)
+        self.music.play()
