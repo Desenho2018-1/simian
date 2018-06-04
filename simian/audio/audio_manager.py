542a06a920a50b88c53adfe6457327e3c8dcd39b
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-04 20:39:36 -0300

    Create add sound method for audio manager class
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com>

--- simian/audio/audio_manager.py
+++ simian/audio/audio_manager.py
@@ -4,7 +4,7 @@ from simian.audio.audios import Audio, Music, Sound
 class AudioManager:
     def __init__(self):
         pygame.mixer.init()
-        self.sounds = []
+        self.sounds = sounds[:8]
         self.music = None
 
     def pause_all(self):
@@ -13,7 +13,23 @@ class AudioManager:
     def unpause_all(self):
         pygame.mixer.unpause()
 
+    """
+    Used to call music class functions and play a single music in an infinite loop
+    """
     def play_music(self, music_path):
         self.music = Music(music_path)
         self.music.play()
         return True
+
+    """
+    Used to add a sound to a list of available sounds that can be used by pygame
+    """
+    def add_sound(self, sound_object):
+        if isinstance(sound_object, Sound):
+            if len(sounds) < 8:
+                self.sounds.append(sound_object)
+                return True
+            else:
+                raise Exception('Maximum number of sounds per object exceeded')
+        else:
+            raise TypeError('Parameter must be a Sound object')
