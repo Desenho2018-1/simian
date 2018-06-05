f17140e799ca567247187ffcd13b25ded77cb9fe
Josue Nascimento <josuetk63@gmail.com>
2018-06-05 11:05:51 -0300

    Remove loop for test

--- simian/gameloop/game_loop.py
+++ simian/gameloop/game_loop.py
@@ -1,26 +1,13 @@
 
+
+from simian.utils.exception.unimplemented_method import NotOverriddenMethod
+from simian.text.text_object import TextObject
 import pygame
+
 class GameEngine(object):
 
     def __init__(self):
         pass
 
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    clock = pygame.time.Clock()
-    font = pygame.font.SysFont("comicsansms", 72)
-
-    text = font.render("Hello, World", True, (0, 128, 0))
-    loop = True
-    while loop:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                loop = True
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                loop = False
-
-        screen.fill((255, 255, 255))
-        screen.blit(text,(320 - text.get_width() // 2, 240 - text.get_height() // 2))
-
-        pygame.display.flip()
-        clock.tick(60)
+    while True:
