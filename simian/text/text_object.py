369df904696e52778ef2d9f26709ac2756c2ad3f
Josue Nascimento <josuetk63@gmail.com>
2018-06-05 11:51:46 -0300

    Create method for make text italic and bold

--- simian/text/text_object.py
+++ simian/text/text_object.py
@@ -1,23 +1,36 @@
-
 import pygame
-class TextObject():
 
-    def __init__(self):
-        pass
+    """
+        This class provide, methods
+        for display and to menage texts
+        on screen and scene
+    """
+
+class TextObject(Object):
+
+    def __init__(self,text_message,size,font_text):
+        self.text_message = text_message
+        self.size = size
+        self.font_text = font_text
+        self.font_object = None
+
+    def set_text_font(self):
+        self.font_object =  pygame.font.SysFont(self.font_text, self.size)
 
 
-    def set_text_font(self,font_text,size_font_text):
-         font = pygame.font.SysFont(font_text, size_font_text)
-         return font
+    def draw_text(self,soft_edges,color):
+        return self.font_object.render(self.text_message, soft_edges, color)
 
-    def draw_text(self,font_object,text_message,soft_edges,color):
-         display_message = font_object.render(text_message, soft_edges, color)
-         return display_message
-
+        """
+            Return all font available on the user machine
+        """
     def available_fonts_on_machine(self):
         all_fonts = pygame.font.get_fonts()
         for font in all_fonts:
             print(font)
 
-    def bold_text(self,is_bold):
-        pygame.font.Font.set_bold(is_bold)
+    def is_bold(self,boolean):
+            self.font_object.set_bold(boolean)
+
+    def is_italic(self,boolean):
+            self.font_object.set_italic(boolean)
