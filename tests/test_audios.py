96b1977a262aa349f026fd09b2eed9f52ef4fe99
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-06 19:07:51 -0300

    Create should not create sound test for Sound class
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabíola Fleury <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Fabíola Fleury <fabiola.mfleury@gmail.com

--- tests/test_audios.py
+++ tests/test_audios.py
@@ -7,13 +7,20 @@ class SoundTest(unittest.TestCase):
     def setUp(self):
         self.audio_manager = AudioManager()
 
-    def test_should create_sound(self):
+    def test_should_create_sound(self):
         try:
             sound = Sound('assets/audio/sound.wav')
             self.assertTrue(True)
         except:
             self.assertTrue(False)
-            
+
+    def test_should_not_create_sound(self):
+        try:
+            sound = Sound('doesnt_exist.wav')
+            self.assertTrue(False)
+        except:
+            self.assertTrue(True)
+
     def test_should_play_sound(self):
         sound = Sound('assets/audio/sound.wav')
         self.assertTrue(sound.play())
