61ac7e48530f59bd5ccb05ec15a0a9590c27ab73
Matheus Joranhezon <mjoranhezon@gmail.com>
2018-06-06 19:30:57 -0300

    Make changes to add_sound and create play_sound in AudioManager class

--- simian/audio/audio_manager.py
+++ simian/audio/audio_manager.py
@@ -25,12 +25,24 @@ class AudioManager:
     """
     Used to add a sound to a list of available sounds that can be used by pygame
     """
-    def add_sound(self, sound_object):
-        if isinstance(sound_object, Sound):
-            if len(sounds) < 8:
-                self.sounds.append(sound_object)
-                return True
-            else:
-                raise Exception('Maximum number of sounds per object exceeded')
+    def add_sound(self, sound_path):
+        try:
+            new_sound = Sound(sound_path)
+        except:
+            raise Exception('Could not create sound object')
+
+        if len(sounds) < 8:
+            self.sounds.append(new_sound)
+            return True
         else:
-            raise TypeError('Parameter must be a Sound object')
+            raise Exception('Maximum number of sounds per object exceeded')
+
+    """
+    Used to play a sound previously added to the list
+    """
+    def play_sound(self, sound_object):
+        if sound_object in self.sounds:
+            current_sound = self.sounds.index(sound_object)
+            self.sounds[current_sound].play()
+        else:
+            raise Exception('Sound Object is not a part of the list')
