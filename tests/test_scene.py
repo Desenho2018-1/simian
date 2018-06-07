83c00a4236bbe7e3f93ea6a820703da34550ff90
Josue Nascimento <josuetk63@gmail.com>
2018-06-07 10:33:35 -0300

    Add tests for exception

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -1,6 +1,7 @@
 
 from simian.scene.base_scene import BaseScene
-
+from simian.utils.exception.unimplemented_method import NotOverriddenMethod
+import unittest
 class BaseSceneTest(unittest.TestCase):
 
     def setUp(self):
@@ -12,10 +13,48 @@ class BaseSceneTest(unittest.TestCase):
 
 
     def test_name_scene(self):
-        name = self.base.name
-        assertEqual(name,'test')
+        name = self.baseScene.name
+        self.assertEqual(name,'test')
 
 
     def test_id_scene(self):
         id = self.baseScene.id
-        assertEqual(id,1)
+        self.assertEqual(id,1)
+
+    def test_unimplemented_method_load(self):
+
+        try :
+            self.baseScene.load()
+        except Exception as message:
+            message_exception = str(message)
+            self.assertEqual("the method load isn't overridden",message_exception)
+
+    def test_unimplemented_method_unload(self):
+
+        try :
+            self.baseScene.unload()
+        except Exception as message:
+            message_exception = str(message)
+            self.assertEqual("the method unload isn't overridden",message_exception)
+
+    def test_unimplemented_method_draw(self):
+
+        grafics = None
+        try :
+            self.baseScene.draw(grafics)
+        except Exception as message:
+            message_exception = str(message)
+            self.assertEqual("the method draw isn't overridden",message_exception)
+
+
+    def test_unimplemented_method_update(self):
+
+        time_elapsed = None
+        try :
+            self.baseScene.update(time_elapsed)
+        except Exception as message:
+            message_exception = str(message)
+            self.assertEqual("the method update isn't overridden",message_exception)
+
+if __name__ == '__main__':
+    unittest.main()
