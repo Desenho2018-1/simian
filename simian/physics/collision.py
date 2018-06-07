0cdb6751f94d23bd26fe514c84ce59dbbd801064
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-06 22:00:24 -0300

    Change collision detection function to use Separating Axis Theorem

--- simian/physics/collision.py
+++ simian/physics/collision.py
@@ -12,11 +12,35 @@ def detect_collision(body_a, body_b):
     Verify if two rigid bodies
     are colliding.
     """
-    # is a box vs box collision
-    if hasattr(body_a, 'size') and hasattr(body_b, 'size'):
-        return box_vs_box(body_a.collider, body_b.collider)
-    else:
-        raise NotImplementedError
+    edges = body_a.collider.edges
+    edges += body_b.collider.edges
+
+    orthogonals = [_orthogonal(e) for e in edges]
+
+    push_vectors = []
+    for orthogonal in orthogonals:
+        separates, push_vec = _is_separating_axis(orthogonal,
+                                                  body_a.collider.vertices,
+                                                  body_b.collider.vertices)
+
+        if separates:
+            # Collision not detected
+            return None
+        else:
+            push_vectors.append(push_vec)
+
+    # smallest push vector
+    minimal_push_vec = min(push_vectors, key=(lambda v: v.dot(v)))
+
+    # assert minimal push vector push body_a away from body_b
+    d = _centers_displacement(body_a.collider.vertices,
+                              body_b.collider.vertices)
+
+    # if it's the same direction, invert
+    if d.dot(minimal_push_vec) > 0:
+        minimal_push_vec = -minimal_push_vec
+
+    return minimal_push_vec
 
 
 def resolve_collision(body_a, body_b):
@@ -38,16 +62,65 @@ def resolve_collision(body_a, body_b):
         body_b.velocity += 1/body_b.mass * impulse
 
 
-def box_vs_box(box_a, box_b):
+def _centers_displacement(vertices1, vertices2):
     """
-    Verify colision between two
-    box colliders (AABB).
+    Return the displacement between the geometric center
+    of vertices of a polygon and another.
     """
-    if box_a.bottom_right.x < box_b.top_left.x or
-    box_a.top_left.x > box_b.bottom_right.x:
-        return True
-    if box_a.bottom_right.y < box_b.top_left.y or
-    box_a.top_left.y > box_b.bottom_right.y:
-        return True
+    sum1_x = 0
+    sum1_y = 0
 
-    return True
+    sum2_x = 0
+    sum2_y = 0
+
+    for v in vertices1:
+        sum1_x += v.x
+        sum1_y += v.y
+
+    for v in vertices2:
+        sum2_x += v.x
+        sum2_y += v.y
+
+    center_1 = Vec2(sum1_x/len(vertices1), sum1_y/len(vertices1))
+    center_2 = Vec2(sum2_x/len(vertices2), sum2_y/len(vertices2))
+
+    return center_2 - center_1
+
+
+def _orthogonal(vector):
+    """
+    Return a 90 degree clockwise rotation
+    of the given vector.
+    """
+    return Vec2(-vector.y, vector.x)
+
+
+def _is_separating_axis(orthogonal, vertices1, vertices2):
+    """
+    Return True and the push vector if orthogonal
+    is a separating axis of vertices1 and vertices2.
+    Otherwise, return False and None.
+    """
+    min1, max1 = float('+inf'), float('-inf')
+    min2, max2 = float('+inf'), float('-inf')
+
+    for v in vertices1:
+        projection = v.dot(orthogonal)
+
+        min1 = min(min1, projection)
+        max1 = max(max1, projection)
+
+    for v in vertices2:
+        projection = v.dot(orthogonal)
+
+        min2 = min(min2, projection)
+        max2 = max(max2, projection)
+
+    if max1 >= min2 and max2 >= min1:
+        d = min(max2 - min1, max1 - min2)
+
+        d_over_orthogonal_squared = d / orthogonal.dot(orthogonal) + 1e-10
+        push_vec = orthogonal * d_over_orthogonal_squared
+        return False, push_vec
+    else:
+        return True, None
