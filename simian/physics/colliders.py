1d9818e4a4eb6b1f61d56fc1c3975883cd68a82c
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-06 21:59:36 -0300

    Add vertices and edges attribute for BoxCollider

--- simian/physics/colliders.py
+++ simian/physics/colliders.py
@@ -13,10 +13,40 @@ class BoxCollider(object):
     of a rigid body, is used to perfofm
     AABB collision detection.
     """
-    __slots__ = ['position', 'size', 'top_left', 'bottom_right']
+    __slots__ = ['position', 'size', 'vertices', 'edges']
 
     def __init__(self, position, size):
         self.position = position
         self.size = size
-        self.top_left = Vec2(position.x - (size.x/2), position.y + (size.y/2))
-        self.bottom_right = Vec2(position.x + (size.x/2), position.y - (size.y/2))
+        self.vertices = self._get_vertices()
+        self.edges = self._get_edges()
+
+    def _get_vertices(self):
+        """
+        Return a list with all vertices
+        ordered in counterclockwise direction.
+        """
+        half_width = self.size.w / 2
+        half_height = self.size.h / 2
+
+        v1 = Vec2(self.position.x - half_width, self.position.y + half_height)
+        v2 = Vec2(self.position.x - half_width, self.position.y - half_height)
+        v3 = Vec2(self.position.x + half_width, self.position.y - half_height)
+        v4 = Vec2(self.position.x + half_width, self.position.y + half_height)
+
+        return [v1, v2, v3, v4]
+
+    def _get_edges(self):
+        """
+        Return the vectors that represent
+        all edges of a polygon with the given
+        vertices.
+        """
+        edges = []
+        N = len(self.vertices)
+
+        for i in range(N):
+            edge = self.vertices[(i + 1) % N] - self.vertices[i]
+            edges.append(edge)
+
+        return edges
