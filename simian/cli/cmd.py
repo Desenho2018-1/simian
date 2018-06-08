77354751c47b348a4c61246f1967f78344c062f3
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-08 01:07:44 -0300

    Change folder name

--- simian/cli/cmd.py
+++ simian/cli/cmd.py
@@ -10,13 +10,14 @@ from cookiecutter.main import cookiecutter
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('option', help='siamian options [startproject, run]')
+    parser.add_argument('file', help='file to run')
 
     args = parser.parse_args()
 
     if args.option == 'startproject':
         template_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        cookiecutter(template_path + '/template')
+        cookiecutter(template_path + '/cookiecutter')
     elif args.option == 'run':
-        os.system(f'python {args.arg_option}')
+        os.system(f'python {args.file}')
     else:
         raise ValueError(f'Invalid command "{args.option}"')
