24580d632f3f8f5bca411dd2e06f17b36cfaf7ff
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-07 23:49:56 -0300

    Change project structure generation

--- simian/utils/project.py
+++ simian/utils/project.py
@@ -7,27 +7,33 @@ def create_project(project_name):
     """
     os.mkdir(project_name)
     os.chdir(project_name)
+
     os.mkdir('scenes')
     os.mkdir('game_objects')
     os.mkdir('assets')
+
+    with open('__init__.py', 'a'):
+        pass
+
     with open('conf.py', 'a') as conffile:
         create_confpy(conffile)
+
     with open('run.py', 'a') as runfile:
         create_runpy(runfile)
 
 
 def create_confpy(conffile):
-    conffile.write('GAME_NAME = "Pong"')
-    conffile.write('WINDOW_SIZE = (800, 600)')
-    conffile.write('SCENE_LIST = []')
-    conffile.write('INITIAL_SCENE = "Menu"')
+    conffile.write('GAME_NAME = "Pong"\n\n')
+    conffile.write('WINDOW_SIZE = (800, 600)\n\n')
+    conffile.write('SCENE_LIST = []\n\n')
+    conffile.write('INITIAL_SCENE = "Menu"\n\n')
 
 
 def create_runpy(runfile):
-    runfile.write('from conf.py import *\n')
+    runfile.write('from conf import *\n')
     runfile.write('from simian.engine import GameEngine\n\n')
-    runfile.write('engine = GameEngine()')
-    runfile.write('engine.load(GAME_NAME, WINDOW_SIZE)')
-    runfile.write('[engine.add(scene) for scene in SCENE_LIST]')
-    runfile.write('engine.set(INITIAL_SCENE)')
-    runfile.write('engine.run()')
+    runfile.write('engine = GameEngine()\n')
+    runfile.write('engine.load(GAME_NAME, WINDOW_SIZE)\n')
+    runfile.write('[engine.add(scene) for scene in SCENE_LIST]\n')
+    runfile.write('engine.set(INITIAL_SCENE)\n')
+    runfile.write('engine.run()\n')
