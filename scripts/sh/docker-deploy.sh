e25318a845978686fd68660eebdf872d4a589b59
Alax Alves <alaxallves@gmail.com>
2018-06-08 13:38:28 -0300

    Configuring simian to deploy in docker

--- scripts/sh/docker-deploy.sh
+++ scripts/sh/docker-deploy.sh
@@ -5,6 +5,7 @@ VERSION=$(python setup.py --version)
 
 echo "Latest Simian version is $VERSION";
 
+# double validation in script and in .travis.yml
 if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
 	echo "Deploying to Docker registry latest Simian...";
 	docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
