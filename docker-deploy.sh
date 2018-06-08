00f0886cd30248318c796e4914bdc80f3c6f7d4c
Alax Alves <alaxallves@gmail.com>
2018-06-08 09:45:33 -0300

    Using travis to deploy continuously latest simian in docker
    Signed-off-by: Alax Alves <alaxallves@gmail.com>
    Signed-off-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Signed-off-by: Fabiola Fleury <fabiola.mfleury@gmail.com>
    
    Co-authored-by: Matheus Joranhezon <mjoranhezon@gmail.com>
    Co-authored-by: Fabiola Fleury <fabiola.mfleury@gmail.com>

---
+++ docker-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Represents the latest version of the project according to setup.py file
+VERSION=$(python setup.py --version)
+
+echo "Latest Simian version is $VERSION";
+
+if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
+	echo "Deploying to Docker registry latest Simian...";
+	docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+	docker build -f docker/Dockerfile -t simian:"$VERSION" .;
+	docker tag alaxalves/simian:"$VERSION";
+	docker push alaxalves/simian:"$VERSION";
+else
+	echo "Skipping Docker registry deploy";
+fi;
