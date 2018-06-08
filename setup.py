db5e7f42574a2f2e48fd0e0eed611f5cb1750dbc
Alax Alves <alaxallves@gmail.com>
2018-06-07 21:12:19 -0300

    Setting up config files to deploy

--- setup.py
+++ setup.py
@@ -4,6 +4,34 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+CLASSIFIERS= [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: Portuguese",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+META_FILE = read(META_PATH)
+
+def read(*parts):
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+def find_meta(meta):
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
 setup(
     name='simian_engine',
 
@@ -22,4 +50,18 @@ setup(
             'simian = simian.cli.cmd:main',
         ],
     },
+
+    classifiers=CLASSIFIERS,
+
+    license=find_meta('license'),
+
+    keywords='',
+
+    author='',
+
+    author_email='',
+
+    maintainer='',
+
+    maintainer_email='',
 )
