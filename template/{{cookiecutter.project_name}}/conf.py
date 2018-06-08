a68ed29cfa2e557d557ad4b0bb96c04191a8fc8f
rodrigocam <rodrigo.redcode@gmail.com>
2018-06-08 01:04:49 -0300

    Add cookicutter to generate game structure

---
+++ template/{{cookiecutter.project_name}}/conf.py
@@ -0,0 +1,7 @@
+GAME_NAME = {{cookiecutter.project_name}}
+
+WINDOW_SIZE = ({{cookiecutter.window_width}}, {{cookiecutter.window_height}})
+
+SCENE_LIST = []
+
+INITIAL_SCENE = {{cookiecutter.initial_scene}}
