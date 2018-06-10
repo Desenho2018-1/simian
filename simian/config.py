3e53b6681850c2eaa7cee20b33e34931c730a6a7
Roger Lenke <rogerlenke@gmail.com>
2018-06-10 02:48:18 -0300

    Correct structure to relative paths

--- simian/config.py
+++ simian/config.py
@@ -31,3 +31,17 @@ class Configuration(object):
             (self.config[self.paths_section][self.image_assets_path]))
 
         self.IMAGE_ASSETS_PATH = self.IMAGE_ASSETS_PATH.strip("'")
+
+    def correct_paths(self):
+        # If the string don't starts with /, it means it wasn't a full path.
+        # If that is the case, then we join the relative path with
+        # this file full path
+        if(not self.AUDIO_ASSETS_PATH.startswith("/")):
+            dirname = os.path.dirname(__file__)
+            self.AUDIO_ASSETS_PATH = os.path.join(dirname,
+                                                  self.AUDIO_ASSETS_PATH)
+
+        if(not self.IMAGE_ASSETS_PATH.startswith("/")):
+            dirname = os.path.dirname(__file__)
+            self.IMAGE_ASSETS_PATH = os.path.join(dirname,
+                                                  self.IMAGE_ASSETS_PATH)
