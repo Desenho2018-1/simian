247dceb88cd8704a1c20d597f9c215086fbb43db
pablodiegoss <pablodiegoss@hotmail.com>
2018-06-11 09:37:46 -0300

    Update template game to have 2 scenes

--- examples/template_scene1.py
+++ examples/template_scene1.py
@@ -1,9 +1,10 @@
-from simian.scene.base_scene import BaseScene
+from simian.scene.base_scene import BaseScene, State
 from simian.input.keyboard_manager import Keyboard
 from examples.template_player_object import Player
+from simian.engine import GameEngine
 
 
-class TemplateScene(BaseScene):
+class TemplateScene1(BaseScene):
     def draw(self, graphics):
         for game_object in self.game_objects:
             game_object.draw(graphics)
@@ -11,10 +12,17 @@ class TemplateScene(BaseScene):
     def update(self, time_elapsed):
         for game_object in self.game_objects:
             game_object.update(time_elapsed)
+        
+        print(self.player.position.x)
+        if(self.player.position.x > 400):
+            GameEngine().set_next_scene("Fase2")
+            self.state = State.FINISHED
+            
 
     def load(self):
         self.keyboard = Keyboard()
-        self.game_objects = [Player(50,50)]
+        self.player = Player(50,50)
+        self.game_objects = [self.player]
 
     def unload(self):
         pass
