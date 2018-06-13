252d0e23b1da1e9da26096db78ba3d41a4e2ed34
Alax Alves <alaxallves@gmail.com>
2018-06-13 14:40:57 -0300

    Fix Docker continuous deploy

--- scripts/sh/docker-deploy.sh
+++ scripts/sh/docker-deploy.sh
@@ -8,10 +8,10 @@ echo "Latest Simian version is $VERSION";
 # double validation in script and in .travis.yml
 if [[ "${TRAVIS_BRANCH}" == "master" ]]; then
 	echo "Deploying to Docker registry latest Simian...";
-	docker login -e "$DOCKER_EMAIL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-	docker build -f docker/Dockerfile -t simian:"$VERSION" .;
-	docker tag alaxalves/simian:"$VERSION";
-	docker push alaxalves/simian:"$VERSION";
+	docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+	docker build -f /home/travis/build/Desenho2018-1/simian/scripts/docker/Dockerfile -t simian:$VERSION .;
+	docker tag simian:$VERSION $DOCKER_USERNAME/simian:$VERSION;
+	docker push $DOCKER_USERNAME/simian:$VERSION;
 else
 	echo "Skipping Docker registry deploy";
 fi;
