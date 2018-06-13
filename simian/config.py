2a8a4545fed5643d6a4115066a4b961d3fe24ff3
Roger Lenke <rogerlenke@gmail.com>
2018-06-13 10:46:43 -0300

    Correct path correction

--- simian/config.py
+++ simian/config.py
@@ -25,23 +25,24 @@ class Configuration(object):
         self.AUDIO_ASSETS_PATH = (
             (self.config[self.paths_section][self.audio_assets_path]))
 
-        self.AUDIO_ASSETS_PATH = self.AUDIO_ASSETS_PATH.strip("'")
-
         self.IMAGE_ASSETS_PATH = (
             (self.config[self.paths_section][self.image_assets_path]))
 
+    def correct_paths(self):
+        self.strip_quotes()
+        self.transform_relative_to_absolute()
+        
+    def strip_quotes(self):
+        self.AUDIO_ASSETS_PATH = self.AUDIO_ASSETS_PATH.strip("'")
         self.IMAGE_ASSETS_PATH = self.IMAGE_ASSETS_PATH.strip("'")
 
-    def correct_paths(self):
-        # If the string don't starts with /, it means it wasn't a full path.
-        # If that is the case, then we join the relative path with
-        # this file full path
-        if(not self.AUDIO_ASSETS_PATH.startswith("/")):
-            dirname = os.path.dirname(__file__)
-            self.AUDIO_ASSETS_PATH = os.path.join(dirname,
-                                                  self.AUDIO_ASSETS_PATH)
+    def transform_relative_to_absolute(self):
+        dirname = os.path.dirname(__file__)
 
-        if(not self.IMAGE_ASSETS_PATH.startswith("/")):
-            dirname = os.path.dirname(__file__)
+        if(not os.path.isabs(self.IMAGE_ASSETS_PATH)):
             self.IMAGE_ASSETS_PATH = os.path.join(dirname,
                                                   self.IMAGE_ASSETS_PATH)
+
+        if(not os.path.isabs(self.AUDIO_ASSETS_PATH)):
+            self.AUDIO_ASSETS_PATH = os.path.join(dirname,
+                                                  self.AUDIO_ASSETS_PATH)
