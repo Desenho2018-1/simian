98f8dce39561bc7692c911f05c4384573b998c78
Hugo Carvalho <hugonvsc@gmail.com>
2018-06-13 01:10:52 -0300

    Add methods to hover and click on game_object

--- simian/input/mouse_manager.py
+++ simian/input/mouse_manager.py
@@ -51,20 +51,41 @@ class Mouse(metaclass=Singleton):
     def get_cursor(self):
         return pygame.mouse.get_cursor()
 
-    #
+    # 
     def set_cursor(size, hotspot, xormasks, andmasks):
         pygame.mouse.set_cursor(size, hotspot, xormasks, andmasks)
 
-    # Set an gameObject to be your new icon
-    def set_custom_cursor(self, gameObject):
+    # Set an game_object to be your new icon
+    def set_custom_cursor(self, game_object):
         self.set_visible(False)
         position = self.get_position()
 
 
-    # Check if mouse is over a gameObject
-    def is_over(self, gameObject):
-        pass
+    # Check if mouse is over a game_object
+    def is_over(self, game_object):
+        collider = game_object.collider
+        mouse_position_x, mouse_position_y = self.get_position()
 
-    # Check if mouse clicked over gameObject
-    def is_clicked(self, gameObject):
-        pass
+        first_point = (collider.position[0] - collider.size[0] / 2 , collider.position[1] - collider.size[1] / 2)
+        second_point = (collider.position[0] + collider.size[0] / 2 , collider.position[1] + collider.size[1] / 2)
+
+        if ((mouse_position_x >= first_point[0] and mouse_position_x <= second_point[0]) and
+           (mouse_position_y >= first_point[1] and mouse_position_y <= second_point[1])):
+           return True
+
+        return False
+
+    # Check if mouse clicked over game_object
+    def is_clicked(self, game_object):
+        is_over = self.is_over(game_object)
+        button_pressed = {'left': False,
+                          'scroll': False,
+                          'right':False}
+
+        if is_over:
+            button_pressed['left'] = self.left_pressed()
+            button_pressed['scroll'] = self.scroll_pressed()
+            button_pressed['right'] = self.right_pressed()
+
+
+        return button_pressed
