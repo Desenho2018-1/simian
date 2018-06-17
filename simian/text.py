e774a8461c4ef5dc4ea2fbd533214a77107a368d
Hugo Carvalho <hugonvsc@gmail.com>
2018-06-17 17:57:58 -0300

    Update text module

--- simian/text.py
+++ simian/text.py
@@ -13,9 +13,10 @@ class Text:
     that can be drawn to the screen.
     """
     def __init__(self, content, size, font='default'):
+        pygame.font.init()
         self.content = content
         self.size = size
-        self._font = pygame.SysFont(font, size)
+        self._font = pygame.font.SysFont(font, size)
         self._styles = {'underline': False, 'bold': False, 'italic': False}
 
     @property
@@ -58,3 +59,16 @@ class Text:
     def italic(self, boolean):
         self._styles['italic'] = boolean
         self._font.set_italic(boolean)
+
+
+    """
+        Methods for check if style is applied to text
+    """
+    def is_underline(self):
+        return self.styles['underline']
+
+    def is_bold(self):
+        return self.styles['bold']
+
+    def is_italic(self):
+        return self.styles['italic']
